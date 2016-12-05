16bb52e53cc2a07ee84fd120f5a4bce232371d35
ChanMo <chen.orange@aliyun.com>
2016-12-05 21:34:38 +0800

    upgrade

--- gulpfile.js
+++ gulpfile.js
@@ -27,8 +27,10 @@ gulp.task('sass', function(){
 
 /** js **/
 gulp.task('js', function(){
-    return gulp.src('src/js/**/*.+(js|json)')
+    return gulp.src(['src/js/**/*.+(js|json)', 'src/components/**/*.+(js|json)'])
         .pipe(gulp.dest('cache/js')) //compile to cache js
+        //.pipe(uglify()) // compress
+        //.pipe(rename('app.min.js')) //rename
         //.pipe(gulp.dest('docs/js'))
         .pipe(browserSync.reload({stream:true}));
 });
@@ -41,6 +43,12 @@ gulp.task('images', function(){
         //.pipe(imagemin())
         //.pipe(gulp.dest('docs/img'));
 });
+gulp.task('dist:images', function(){
+    return gulp.src('src/img/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('docs/img'));
+});
+
 
 /** fonts **/
 gulp.task('fonts', function(){
@@ -48,10 +56,20 @@ gulp.task('fonts', function(){
         .pipe(gulp.dest('cache/fonts')); // copy to cache
         //.pipe(gulp.dest('docs/fonts'));
 });
+gulp.task('dist:fonts', function(){
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('docs/fonts'));
+});
+
 
 /** clean for build **/
-gulp.task('clean', function(){
-    return del.sync(['docs', 'cache']);
+gulp.task('clean:docs', function(){
+    return del.sync('docs');
+});
+
+/** clean cache **/
+gulp.task('clean:cache', function(){
+    return del.sync('cache');
 });
 
 
@@ -64,51 +82,40 @@ gulp.task('useref', function(){
         .pipe(gulp.dest('docs'));
 });
 
-/** page **/
-gulp.task('page', function(){
-  return gulp.src('src/page/**/*.html')
-    .pipe(gulp.dest('src/templates'))
-    .pipe(browserSync.reload({stream:true}));
-});
-
-/** components **/
-gulp.task('components', function(){
-  return gulp.src('src/components/**/*.html')
-    .pipe(gulp.dest('src/templates'))
-    .pipe(browserSync.reload({stream:true}));
-});
-
 /** nunjucksRender **/
 gulp.task('nunjucksRender', function(){
-    return gulp.src('src/templates/*.html')
+    //return gulp.src(['src/templates/*.html','src/pages/**/*.html', 'src/components/**/*.html'])
+    return gulp.src('src/**/*.html')
         .pipe(nunjucksRender({
-            path: 'src/templates'
+            path: 'src'
         }))
         .pipe(gulp.dest('cache'))
         .pipe(browserSync.reload({stream:true}));
+});
+gulp.task('dist:html', function(){
+    return gulp.src('cache/**/*.html')
+        .pipe(gulp.dest('docs'))
 });
 
 /** browserSync **/
 gulp.task('browserSync', function(){
     browserSync.init({
         server: {
-            //baseDir: 'cache'
-          baseDir: 'src'
+            baseDir: 'cache'
         }
     });
 });
 
 /** default **/
-gulp.task('default', ['sass', 'js', 'nunjucksRender', 'browserSync'], function(){
+gulp.task('default', ['clean:cache', 'sass', 'js', 'fonts', 'images', 'nunjucksRender', 'browserSync'], function(){
     console.log('Hello Chen');
-    //gulp.watch('src/page/**/*.html', ['page']);
-    //gulp.watch('src/components/**/*.html', ['components']);
-    gulp.watch(['src/scss/**/*.scss','src/page/**/*.scss','src/components/**/*.scss'], ['sass']);
+    gulp.watch(['src/scss/**/*.scss','src/pages/**/*.scss','src/components/**/*.scss'], ['sass']);
     gulp.watch(['src/js/*.js','src/page/**/*.js','src/components/**/*.js'], ['js']);
-    gulp.watch('src/templates/**/*.html', ['nunjucksRender']);
+    //gulp.watch(['src/templates/**/*.html', 'src/pages/**/*.html'], ['nunjucksRender']);
+    gulp.watch('src/**/*.html', ['nunjucksRender']);
 });
 
 /** build **/
-gulp.task('build', ['clean', 'sass', 'js', 'nunjucksRender', 'fonts', 'images', 'useref'], function(){
+gulp.task('build', ['clean:docs', 'sass', 'js', 'nunjucksRender', 'dist:fonts', 'dist:images', 'dist:html', 'useref'], function(){
     console.log('Building files...');
 })
